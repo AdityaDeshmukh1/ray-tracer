5bb6ded5489d863105031dedae0c2cca9b4a4376
Aditya Deshmukh <adityad1405@gmail.com>
2025-12-07 05:19:45 +0530

    feat: add max depth for ray bounces

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 #include "sphere.h"
 
 int main() {
-	
+
 	// World
-	
+
 	hittable_list world;
 
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
@@ -20,6 +20,7 @@ int main() {
 	cam.aspect_ratio = 16.0 / 9.0;
 	cam.image_width = 400;
 	cam.samples_per_pixel = 100;
+	cam.max_depth = 50;
 
 	cam.render(world);
 
